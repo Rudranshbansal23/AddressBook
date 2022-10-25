61aab76da27d2fbb23b1615274a123f715046d92
Rudranshbansal23 <bansalrudransh2@gmail.com>
2022-10-25 17:53:15 +0530

    Added: Ability to read and write the adress book with persons contact

--- src/com/bridgelabz/addressbook/Contacts.java
+++ src/com/bridgelabz/addressbook/Contacts.java
@@ -1,88 +1,95 @@
 package com.bridgelabz.addressbook;
 
-class Contacts {
-    private String firstName, lastName, address, city, state, email;
-    private long zip;
-    private long phoneNumber;
+class Contact {
+	String firstName;
+	String lastName;
+	String address;
+	String city;
+	String state;
+	Long zipCode;
+	Long phoneNumber;
+	String email;
 
-    public Contacts (String firstName, String lastName, String address, String city, String state, Long zip,
-                     Long phoneNumber, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-    }
+	public Contact(String firstName, String lastName, String address, String city, String state, Long zipCode,
+			Long phoneNumber, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zipCode = zipCode;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
 
-    public String getFirstName() {
-        return firstName;
-    }
+	@Override
+	public String toString() {
+		return "Contacts [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
+				+ ", state=" + state + ", zipCode=" + zipCode + ", phoneNumber=" + phoneNumber + ", email=" + email
+				+ "]";
+	}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public String getCity() {
+		return city;
+	}
 
-    public String getAddress() {
-        return address;
-    }
+	public void setCity(String city) {
+		this.city = city;
+	}
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+	public String getState() {
+		return state;
+	}
 
-    public String getCity() {
-        return city;
-    }
+	public void setState(String state) {
+		this.state = state;
+	}
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public String getState() {
-        return state;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public void setState(String state) {
-        this.state = state;
-    }
+	public String getAddress() {
+		return address;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public Long getZipCode() {
+		return zipCode;
+	}
 
-    public long getZip() {
-        return zip;
-    }
+	public void setZipCode(Long zipCode) {
+		this.zipCode = zipCode;
+	}
 
-    public void setZip(long zip) {
-        this.zip = zip;
-    }
+	public Long getPhoneNumber() {
+		return phoneNumber;
+	}
 
-    public long getPhoneNumber() {
-        return phoneNumber;
-    }
+	public void setPhoneNumber(Long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
 
-    public void setPhoneNumber(long phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String toString() {
-        return ("First name: " + firstName + " Last name: " + lastName + " Address: " + address + " city: " + city
-                + " state: " + state + " email: " + email + " zip: " + zip + " phone number:" + phoneNumber + "");
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }

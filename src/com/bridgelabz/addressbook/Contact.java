05c91ffc29f2bdad3f82452ae9394cc66ce7f03d
Rudranshbansal23 <bansalrudransh2@gmail.com>
2022-10-25 21:20:00 +0530

    Added:Functionality to insert new Contact data into database

---
+++ src/com/bridgelabz/addressbook/Contact.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.addressbook;
+
+import java.util.Scanner;
+
+public class Contact {
+	Scanner sc = new Scanner(System.in);
+	String first, last, address, city, state, zip, phno, email, DOJ;
+	int id;
+
+	public boolean equals(Contact o) {
+		if (o.first.equalsIgnoreCase(this.first) && o.last.equalsIgnoreCase(this.last)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
